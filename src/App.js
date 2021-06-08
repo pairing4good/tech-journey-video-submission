fedfb1965b58c3ceb8eaa712700c7d631b9395c1
dkessler <dkessler@pillartechnology.com>
2021-06-08 16:05:36 -0500

    added validation

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { withAuthenticator } from '@aws-amplify/ui-react';
 import { createSubmission } from './graphql/mutations';
 import { Auth } from 'aws-amplify';
 import { v4 as uuidv4 } from 'uuid';
-import { Row, Col, Form, Button, Spinner } from 'react-bootstrap';
+import { Row, Col, Form, Button, Spinner, FormFile} from 'react-bootstrap';
 
 const initialFormState = { 
   firstName: '',
@@ -18,6 +18,7 @@ function App() {
   const videoSubmission = useRef(null);
   const [state, setState] = useState({isSubmitting: false});
   const [formData, setFormData] = useState(initialFormState);
+  const [validated, setValidated] = useState(false);
 
   useEffect(() => {
     Auth.currentAuthenticatedUser().then( user => {
@@ -40,28 +41,43 @@ function App() {
     setState({isSubmitting: false});
   }
 
+  const handleSubmit = async (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+      setValidated(true);
+    }else{
+      save()
+      setValidated(false);
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  };
+
   return (
     <div className="App">
       <Row>
         <Col>
-          <Form>
+          <Form noValidate validated={validated} onSubmit={handleSubmit}>
             <Form.Group>
               <Form.Control required type="text" onChange={e => setFormData({ ...formData, 'firstName': e.target.value})}
                 placeholder="First Name"
                 value={formData.firstName}/>
-              <Form.Control.Feedback>Please provide your first name.</Form.Control.Feedback>
+              <Form.Control.Feedback  type="invalid">Please provide your first name.</Form.Control.Feedback>
             </Form.Group>
             <Form.Group>
-              <Form.Control type="text" onChange={e => setFormData({ ...formData, 'lastName': e.target.value})}
+              <Form.Control required type="text" onChange={e => setFormData({ ...formData, 'lastName': e.target.value})}
                 placeholder="Last Name"
                 value={formData.lastName}/>
+                <Form.Control.Feedback  type="invalid">Please provide your last name.</Form.Control.Feedback>
             </Form.Group>
             <Form.Group>
-            <Form.Control
+            <Form.Control required
                 as="select"
                 value={formData.schoolGrade}
                 onChange={e => setFormData({ ...formData, 'schoolGrade': e.target.value})}>
-                <option value="">--Select--</option>
+                <option value="">School Grade</option>
                 <option value="7">7th</option>
                 <option value="8">8th</option>
                 <option value="9">9th</option>
@@ -69,13 +85,17 @@ function App() {
                 <option value="11">11th</option>
                 <option value="12">12th</option>
               </Form.Control>
+              <Form.Control.Feedback  type="invalid">Please provide your school grade.</Form.Control.Feedback>
             </Form.Group>
             <Form.Group>
-              <Form.File ref={ videoSubmission }/>
+              <FormFile>
+                <FormFile.Input required ref={ videoSubmission }/>
+                <Form.Control.Feedback  type="invalid">Please provide a 1 minute video.</Form.Control.Feedback>
+              </FormFile>
             </Form.Group>
             <Form.Group>
               {state.isSubmitting ? <Spinner animation="border" variant="primary"/> :
-                <Button onClick={save}>
+                <Button type="submit">
                    Submit Video
                 </Button>}
             </Form.Group>
